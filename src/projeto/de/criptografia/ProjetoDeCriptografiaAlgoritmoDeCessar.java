cbfae6267e00bec36d94b9b4b5135b6e6bc1c032
Angelo Costa Neto <angelo_costa_neto@outlook.com>
2017-09-24 21:15:01 -0300

    adicionado o algoritmo de cesar

--- src/projeto/de/criptografia/ProjetoDeCriptografiaAlgoritmoDeCessar.java
+++ src/projeto/de/criptografia/ProjetoDeCriptografiaAlgoritmoDeCessar.java
@@ -13,8 +13,42 @@ import java.awt.image.BufferedImage;
  */
 public class ProjetoDeCriptografiaAlgoritmoDeCessar {
     
+    public static String encodeCesar(String msg, int key){        
+        int index;
+        String newMessage = "";
+        String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        
+        // Regex all numeric and special character in  string and converts to upper case
+        for(char letter: msg.toUpperCase().replaceAll("[^a-zA-Z]","").toCharArray()){
+            index = new String(alphabet).indexOf(letter);
+            newMessage += Character.toString(alphabet.charAt((index + key)%26));   
+        }
+        
+        return newMessage;
+    }
+    
+    public static String decodeCesar(String msg, int key){
+        int index, value;
+        String newMessage = "";
+        String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        
+        // Regex all numeric and special character in  string and converts to upper case
+        for(char letter: msg.toUpperCase().replaceAll("[^a-zA-Z]","").toCharArray()){
+            index = new String(alphabet).indexOf(letter);
+            if((index - key) < 0){
+               value = 26 + (index - key);
+               newMessage += Character.toString(alphabet.charAt(value%26));
+            }
+            else {
+              newMessage += Character.toString(alphabet.charAt((index - key)%26));  
+            }
+        }
+        
+        return newMessage;
+    }
+    
     public void steganography(String msg, BufferedImage picture) {
-        int height, width, i, j;
+        int height, width, i, j, rgb;
         
         // Transform a string into byte.
         byte[] msg_byte = msg.getBytes();
@@ -27,7 +61,8 @@ public class ProjetoDeCriptografiaAlgoritmoDeCessar {
         
         for (i = 0; i < height; i++) {
             for (j = 0; j< width; j++) {
-                picture.getRGB(i, j);
+                rgb = picture.getRGB(i, j);
+                
             } 
         }
         
